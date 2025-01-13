ecd99f23321cec9f97ec0079ef9488fa824fdbfa
Javed Khan (Node Dev) <javed.khan@technosoftwares.com>
2025-01-13 18:07:46 +0530

    Changes env file

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8000;
-
+require('dotenv').config();
 
 app.get('/api', (req, res) => {
-    res.send('Hi, Javed khan here');
+    res.send('Hi, Javed khan here...............');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
