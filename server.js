e80a7a6feba0c87d035606082d703ca0d08b2e1a
Javed Khan (Node Dev) <javed.khan@technosoftwares.com>
2025-01-13 18:56:10 +0530

    ssssss

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 
 app.get('/api', (req, res) => {
-    res.send('Hi, Javed khan here.............what is');
+    res.send('Hi, Javed khan here.............what is.........');
 });
 
 app.listen(process.env.PORT, () => {
